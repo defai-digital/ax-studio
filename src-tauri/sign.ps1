7adb002026035201735d915ef7b52662421964c8
Bhaskar Vora <bhaskar.vora@defai.digital>
2026-06-22 19:30:49 -0400

    Allow unsigned Windows release builds

--- src-tauri/sign.ps1
+++ src-tauri/sign.ps1
@@ -2,6 +2,26 @@ param (
   [string]$Target
 )
 
+$requiredEnv = @(
+  "AZURE_KEY_VAULT_URI",
+  "AZURE_CLIENT_ID",
+  "AZURE_TENANT_ID",
+  "AZURE_CLIENT_SECRET",
+  "AZURE_CERT_NAME"
+)
+
+foreach ($name in $requiredEnv) {
+  if (-not [Environment]::GetEnvironmentVariable($name)) {
+    Write-Warning "Skipping Windows code signing because $name is not configured."
+    exit 0
+  }
+}
+
+if (-not (Get-Command AzureSignTool.exe -ErrorAction SilentlyContinue)) {
+  Write-Warning "Skipping Windows code signing because AzureSignTool.exe is not available."
+  exit 0
+}
+
 AzureSignTool.exe sign `
   -tr http://timestamp.digicert.com `
   -kvu $env:AZURE_KEY_VAULT_URI `
